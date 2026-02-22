f304a731258e87e1a2592f3e8a056c78290f2df5
emant <joshua999x@gmail.com>
2026-02-22 17:08:46 +0100

    chore: fix spell checker issues

--- src/sparse_matrix.h
+++ src/sparse_matrix.h
@@ -5,37 +5,44 @@
 #include <iostream>
 #include <algorithm>
 
-template<typename Traits>
-class SparseMatrix {
+template <typename Traits>
+class SparseMatrix
+{
 public:
     using Scalar = typename Traits::Scalar;
-    
-    struct Entry {
+
+    struct Entry
+    {
         Scalar val;
         unsigned long col;
         Entry *next;
-        Entry(const Scalar& v, unsigned long c, Entry* n = nullptr)
+        Entry(const Scalar &v, unsigned long c, Entry *n = nullptr)
             : val(v), col(c), next(n) {}
     };
-    
-    struct DiagEntry {
+
+    struct DiagEntry
+    {
         Scalar val;
         Entry *first;
         DiagEntry() : first(nullptr) {}
     };
-    
+
     unsigned long N;
     DiagEntry *diag;
-    
+
     SparseMatrix() : N(0), diag(nullptr) {}
-    
-    SparseMatrix(unsigned long n) : N(n) {
+
+    SparseMatrix(unsigned long n) : N(n)
+    {
         diag = new DiagEntry[N]();
     }
-    
-    ~SparseMatrix() {
-        for (unsigned long i = 0; i < N; i++) {
-            while (diag[i].first) {
+
+    ~SparseMatrix()
+    {
+        for (unsigned long i = 0; i < N; i++)
+        {
+            while (diag[i].first)
+            {
                 Entry *del = diag[i].first;
                 diag[i].first = del->next;
                 delete del;
@@ -43,96 +50,139 @@ public:
         }
         delete[] diag;
     }
-    
-    void setValue(unsigned long i, unsigned long j, const Scalar& valu) {
-        if (Traits::enforces_symmetric_storage && i > j) {
+
+    void setValue(unsigned long i, unsigned long j, const Scalar &value)
+    {
+        if (Traits::enforces_symmetric_storage && i > j)
+        {
             std::swap(i, j);
         }
-        if (j >= N) return;
-        
-        if (i == j) {
-            diag[i].val = valu;
-        } else if (diag[i].first == nullptr) {
-            diag[i].first = new Entry(valu, j);
-        } else {
+        if (j >= N)
+            return;
+
+        if (i == j)
+        {
+            diag[i].val = value;
+        }
+        else if (diag[i].first == nullptr)
+        {
+            diag[i].first = new Entry(value, j);
+        }
+        else
+        {
             Entry *curr = diag[i].first, *prev = nullptr;
-            while (curr && curr->col < j) {
+            while (curr && curr->col < j)
+            {
                 prev = curr;
                 curr = curr->next;
             }
-            
-            if (!curr) {
-                prev->next = new Entry(valu, j);
-            } else if (curr->col == j) {
-                curr->val = valu;
-            } else {
-                if (prev) {
-                    prev->next = new Entry(valu, j, curr);
-                } else {
-                    diag[i].first = new Entry(valu, j, curr);
+
+            if (!curr)
+            {
+                prev->next = new Entry(value, j);
+            }
+            else if (curr->col == j)
+            {
+                curr->val = value;
+            }
+            else
+            {
+                if (prev)
+                {
+                    prev->next = new Entry(value, j, curr);
+                }
+                else
+                {
+                    diag[i].first = new Entry(value, j, curr);
                 }
             }
         }
     }
-    
-    void addToValue(unsigned long i, unsigned long j, const Scalar& valu) {
-        if (Traits::enforces_symmetric_storage && i > j) {
+
+    void addToValue(unsigned long i, unsigned long j, const Scalar &value)
+    {
+        if (Traits::enforces_symmetric_storage && i > j)
+        {
             std::swap(i, j);
         }
-        if (j >= N) return;
-        
-        if (i == j) {
-            diag[i].val += valu;
-        } else if (diag[i].first == nullptr) {
-            diag[i].first = new Entry(valu, j);
-        } else {
+        if (j >= N)
+            return;
+
+        if (i == j)
+        {
+            diag[i].val += value;
+        }
+        else if (diag[i].first == nullptr)
+        {
+            diag[i].first = new Entry(value, j);
+        }
+        else
+        {
             Entry *curr = diag[i].first, *prev = nullptr;
-            while (curr && curr->col < j) {
+            while (curr && curr->col < j)
+            {
                 prev = curr;
                 curr = curr->next;
             }
-            
-            if (!curr) {
-                prev->next = new Entry(valu, j);
-            } else if (curr->col == j) {
-                curr->val += valu;
-            } else {
-                if (prev) {
-                    prev->next = new Entry(valu, j, curr);
-                } else {
-                    diag[i].first = new Entry(valu, j, curr);
+
+            if (!curr)
+            {
+                prev->next = new Entry(value, j);
+            }
+            else if (curr->col == j)
+            {
+                curr->val += value;
+            }
+            else
+            {
+                if (prev)
+                {
+                    prev->next = new Entry(value, j, curr);
+                }
+                else
+                {
+                    diag[i].first = new Entry(value, j, curr);
                 }
             }
         }
     }
-    
-    void gaxpy(Scalar *V, Scalar *R) {
-        for (unsigned long i = 0; i < N; i++) {
+
+    void gaxpy(Scalar *V, Scalar *R)
+    {
+        for (unsigned long i = 0; i < N; i++)
+        {
             R[i] = diag[i].val * V[i];
         }
-        
-        for (unsigned long i = 0; i < N; i++) {
+
+        for (unsigned long i = 0; i < N; i++)
+        {
             Entry *curr = diag[i].first;
-            while (curr) {
+            while (curr)
+            {
                 R[i] += curr->val * V[curr->col];
                 R[curr->col] += curr->val * V[i];
                 curr = curr->next;
             }
         }
     }
-    
-    void printOut() {
-        std::cout << std::endl << std::endl;
-        for (unsigned long i = 0; i < N; i++) {
+
+    void printOut()
+    {
+        std::cout << std::endl
+                  << std::endl;
+        for (unsigned long i = 0; i < N; i++)
+        {
             std::cout << i << " - <[" << diag[i].val << "]>";
             Entry *p = diag[i].first;
-            while (p) {
+            while (p)
+            {
                 std::cout << "->[" << p->val << "]," << p->col;
                 p = p->next;
             }
             std::cout << std::endl;
         }
-        std::cout << std::endl << std::endl;
+        std::cout << std::endl
+                  << std::endl;
     }
 };
 
